80649520f8e5fe1e9e2683dd22ab9d2ddfd3d7ab
javi <javiruizar@gmail.com>
2023-07-08 23:21:48 +0200

    loading page. Pendingo to be tested in cluster

---
+++ streamlit/pages/1_loading_page.py
@@ -0,0 +1,52 @@
+import os.path
+import pathlib
+import streamlit as st
+import subprocess
+import sys
+
+st.write("""
+# Carga de Ficheros
+""")
+st.write("""
+## Precargar fichero a File System
+""")
+uploaded_file = st.file_uploader("Choose a JSON file")
+# if uploaded_file is not None:
+    # bytes_data = uploaded_file.getvalue()
+    # data = uploaded_file.getvalue().decode('utf-8').splitlines()
+    # st.session_state["preview"] = ''
+    #for i in range(0, min(5, len(data))):
+        # st.session_state["preview"] += data[i]
+# preview = st.text_area("CSV Preview", "", height=150, key="preview")
+upload_state = st.text_area("Estado Fichero", "", key="upload_state")
+def upload():
+    if uploaded_file is None:
+        st.session_state["upload_state"] = "Seleccione primero un fichero para subir"
+    else:
+        data = uploaded_file.getvalue().decode('utf-8')
+        parent_path = pathlib.Path(__file__).parent.parent.resolve()
+        save_path = os.path.join(parent_path, "data")
+        complete_name = os.path.join(save_path, uploaded_file.name)
+        destination_file = open(complete_name, "w")
+        destination_file.write(data)
+        destination_file.close()
+        st.session_state["upload_state"] = "El fichero se ha cargado correctamente en " + complete_name
+st.button("Subir fichero a  File System", on_click=upload)
+
+st.write("""
+## Subir fichero a HDFS
+""")
+def uploadToHDFS():
+    subprocess.run([f"{sys.executable}", "/home/bin/putFileToHDFS.sh"])
+    st.write("Fichero subido. Revise HDFS.")
+FsToHdfs = st.button("Subir a HDFS", on_click=uploadToHDFS)
+
+st.write("""
+## Cargar informacion en base de datos
+""")
+
+def loadInfoToMongo():
+    subprocess.run([f"{sys.executable}", "/home/bin/run_spark_batch.sh"])
+    st.write("Fichero subido. Revise HDFS.")
+
+loadToMongo = st.button("Cargar en base de datos", on_click=run-spark-batch.sh)
--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -1,60 +1,28 @@
-from json import dumps, loads
+from json import dumps
 
 from kafka import KafkaProducer
 from kafka import KafkaConsumer
-# from pykafka.common import OffsetType
 #pip install kafka-python
 
 
 import streamlit as st
-# from pykafka import KafkaClient
 
-# client = KafkaClient(hosts='localhost:9095')
-# inputTopic = client.topics[b'streaming-query']
-# outputTopic = client.topics[b'output']
-# inputProducer = inputTopic.get_sync_producer()
-# outputConsumer = outputTopic.get_simple_consumer(auto_offset_reset=OffsetType.LATEST,
-#     reset_offset_on_start=True)
-# consumer = outputTopic.
-# import requests
-# from streamlit_lottie import st_lottie
-# from PIL import Image
-# from collections import Counter
-#from confluent_kafka import Producer
 
 st.title('Bienvenido a ElMercado.com')
 st.header('Estás en la seccion de busqueda')
 st.subheader('Busca aqui el artículo que quieras comprar')
 articulo= st.text_input("Describe tu articulo")
 # tienda = st.text_input("Quieres comprar en una tienda en particular? Introduce el nombre de la tienda")
-buy = st.button('BUSCAR', 'key1', 'this button is used to buy stuff')
 # initializing the Kafka producer
 my_producer = KafkaProducer(
-    bootstrap_servers = ['localhost:9095'],
+    bootstrap_servers = ['workernode1:9092'],
     value_serializer = lambda x:dumps(x).encode('utf-8')
     )
-my_producer.send('streaming-query', value = articulo)
-# if (articulo):
-	# if(tienda):
-	# 	#send_message(mensaje)
-	# 	st.text(f"Quieres comprar un {articulo} en {tienda}")
-	# 	st.caption("Aqui tienes las posibilidades. Haz click sobre una para acceder a la seccion de compra")
-	# else:
-	# 	st.text(f"Quieres comprar un {articulo} en cualquier tienda")
-	# 	st.caption("Aqui tienes las posibilidades. Haz click sobre una para acceder a la seccion de compra")
+def sendMessage ():
+    my_producer.send('streaming-query', value = articulo)
 
-# mensaje = articulo.encode(encoding='UTF-8')
-# inputProducer.produce(mensaje)
-# consumer = outputConsumer.get_simple_consumer(
-#     auto_offset_reset=OffsetType.LATEST,
-#     reset_offset_on_start=True)
-# st.text(outputConsumer)
-#def send_message(message):
-#    p = Producer({'bootstrap.servers': 'localhost:9092'})
-#    topic = 'mi-topico'  # Reemplaza con el nombre de tu tópico
-#    p.produce(topic, message.encode('utf-8'))
-#    p.flush()
-# mytext = "este es un texto en una variable"
+buy = st.button('BUSCAR', 'key1', 'this button is used to buy stuff', on_click=sendMessage)
+
 # st.text(mytext)
 # st.text('this is text')
 # st.write('this is the write section')
@@ -65,18 +33,13 @@ my_producer.send('streaming-query', value = articulo)
 # generating the Kafka Consumer
 my_consumer = KafkaConsumer(
     'output',
-     bootstrap_servers = ['localhost : 9095'],
+     bootstrap_servers = ['workernode1:9092'],
      auto_offset_reset = 'latest',
      enable_auto_commit = False,
      # group_id = 'my-group',
      # value_deserializer = lambda x : loads(x.decode('utf-8'))
      )
-# a = my_consumer(0)
-# st.text(a)
-# lista = ["1","2","3"]
-# for i in lista:
-# 	st.text(i)
-# mensajes = []
+
 st.text("resultados para '" + articulo + "'")
 for message in my_consumer:
 	mensaje = message.value.decode('utf-8').strip("'")
