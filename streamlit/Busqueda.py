8c6572f323cc19d2078bc05ed9114c71031a6c03
javi <javiruizar@gmail.com>
2023-09-01 01:16:31 +0200

    Clean unused statements and comments

--- streamlit/Busqueda.py
+++ streamlit/Busqueda.py
@@ -2,8 +2,6 @@ from json import dumps
 
 from kafka import KafkaProducer
 from kafka import KafkaConsumer
-#pip install kafka-python
-
 
 import streamlit as st
 
@@ -12,8 +10,7 @@ st.title('Bienvenido a ElMercado.com')
 st.header('Estás en la seccion de busqueda')
 st.subheader('Busca aqui el artículo que quieras comprar')
 articulo= st.text_input("Describe tu articulo y pulsa enter")
-# tienda = st.text_input("Quieres comprar en una tienda en particular? Introduce el nombre de la tienda")
-# initializing the Kafka producer
+
 my_producer = KafkaProducer(
     bootstrap_servers = ['workernode1:9092'],
     value_serializer = lambda x:dumps(x).encode('utf-8')
@@ -21,15 +18,6 @@ my_producer = KafkaProducer(
 
 my_producer.send('streaming-query', value = articulo)
 
-# buy = st.button('BUSCAR', 'key1', 'this button is used to buy stuff', on_click=sendMessage)
-
-# st.text(mytext)
-# st.text('this is text')
-# st.write('this is the write section')
-# "What about this string? Just magic, its like .write?"
-# st.divider()
-# st.code("val a = 'This is the  code Section, divided with .divider() functino.  where you can scape using \\ character'")
-# st.caption('This is caption, to write small text')
 # generating the Kafka Consumer
 my_consumer = KafkaConsumer(
     'output',
@@ -44,6 +32,3 @@ st.text("resultados para '" + articulo + "'")
 for message in my_consumer:
 	mensaje = message.value.decode('utf-8').strip("'")
 	st.text((mensaje))
-	# mensajes.append(mensaje)
-# st.text("un testo")
-# st.text(my_consumer.poll())
